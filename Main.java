b98b8569ea708d87c58f58029d92f1c6d4eb0398
Nipaaaa1 <frostone224@gmail.com>
2026-02-09 14:59:01 +0000

    1: Print statement,

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main() {
+    System.out.println("Hello from Java!");
+    System.out.println("I'm learning Java!");
+    System.out.println("It's cool!");
+  }
+}
