5e690088b951cc022c0214aebb7ba05193ad466c
Nipaaaa1 <frostone224@gmail.com>
2026-02-09 15:05:33 +0000

    2: variables

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
 public class Main {
   public static void main() {
-    System.out.println("Hello from Java!");
-    System.out.println("I'm learning Java!");
-    System.out.println("It's cool!");
+    int age = 20;
+    String name = "Nipa";
+    char grade = 'S';
+    boolean isProgrammer = true;
+    double salary = 99;
+
+    System.out.println("My name is: " + name);
+    System.out.println("I'm " + age + " years old");
+    System.out.println("My average grade: " + grade);
+    System.out.println("My salary is: " + salary);
+
+    if (isProgrammer) {
+      System.out.println("I'm a programmer");
+    } else {
+      System.out.println("I'm not a programmer");
+    }
+
   }
 }
