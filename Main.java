a75d26303925fac59d775639695446fa0362dcda
Nipaaaa1 <frostone224@gmail.com>
2026-02-10 20:51:45 +0000

    11: enhanced switch

--- Main.java
+++ Main.java
@@ -4,16 +4,18 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    // Temperature converter
+    // 11 - Enhanced switch
 
-    System.out.print("Enter your temperature: ");
-    double temp = scanner.nextDouble();
-    System.out.print("Convert to celcius or fahrenheit (C or F): ");
-    String choice = scanner.next().toUpperCase();
+    System.out.print("Enter your day: ");
+    String day = scanner.nextLine();
 
-    double newTemp = (choice.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
-
-    System.out.printf("Your temperature is %.2f%s\n", newTemp, choice);
+    switch (day) {
+      case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
+        System.out.println("It's weekday😭");
+      case "Saturday", "Sunday" ->
+        System.out.println("It's a weekend😁");
+      default -> System.out.println(day + " is not a day");
+    }
 
     scanner.close();
   }
