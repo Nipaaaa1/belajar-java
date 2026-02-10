e20c1680793a5c22e9da5777577ae7b3f6f0f84f
Nipaaaa1 <frostone224@gmail.com>
2026-02-10 20:41:24 +0000

    10: temperature converter

--- Main.java
+++ Main.java
@@ -4,27 +4,16 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    // Weight conversion program
+    // Temperature converter
 
-    System.out.println("Weight conversion program");
-    System.out.println("1: convert pounds to kilograms");
-    System.out.println("2: convert kilograms to pounds");
-    System.out.print("Choose menu (1 or 2): ");
-    int choice = scanner.nextInt();
+    System.out.print("Enter your temperature: ");
+    double temp = scanner.nextDouble();
+    System.out.print("Convert to celcius or fahrenheit (C or F): ");
+    String choice = scanner.next().toUpperCase();
 
-    if (choice == 1) {
-      System.out.print("Enter the weight it pounds: ");
-      double weight = scanner.nextDouble();
-      double newWeight = weight * 0.453592;
-      System.out.printf("The weight in kilograms is: %.2f\n", newWeight);
-    } else if (choice == 2) {
-      System.out.print("Enter the weight it kilograms: ");
-      double weight = scanner.nextDouble();
-      double newWeight = weight * 2.20462;
-      System.out.printf("The weight in pounds is: %.2f\n", newWeight);
-    } else {
-      System.out.println("Please choose the correct number.");
-    }
+    double newTemp = (choice.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+    System.out.printf("Your temperature is %.2f%s\n", newTemp, choice);
 
     scanner.close();
   }
