e14c779eaa5c0f6f587a49b0ecb56a979a490989
Nipaaaa1 <frostone224@gmail.com>
2026-02-11 01:35:13 +0000

    21: varargs

--- Main.java
+++ Main.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    // 20 - Arrays assignment
+    // 21 - varargs
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Enter # of fruits: ");
-    int size = scanner.nextInt();
-    scanner.nextLine(); // Clear buffer from newline
-
-    String[] fruits = new String[size];
-
-    for (int i = 0; i < fruits.length; i++) {
-      System.out.print("Enter the fruit name: ");
-      fruits[i] = scanner.nextLine();
-    }
-
-    for (String fruit : fruits) {
-      System.out.println(fruit);
-    }
+    System.out.println(average(1, 2, 3, 4, 5, 6, 7));
 
     scanner.close();
 
+  }
+
+  static double average(double... numbers) {
+    if (numbers.length == 0) {
+      return 0;
+    }
+
+    double sum = 0;
+    for (double number : numbers) {
+      sum += number;
+    }
+
+    return sum / numbers.length;
   }
 }
