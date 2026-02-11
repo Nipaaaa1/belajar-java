5903c0b4789571a1d2033ce6204613f722862a64
Nipaaaa1 <frostone224@gmail.com>
2026-02-11 19:57:08 +0000

    36: date time

--- Main.java
+++ Main.java
@@ -1,25 +1,50 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
   public static void main(String[] args) {
-    // 35 - File reading
+    // 36 - Datetime
 
-    String filePath = "file.txt";
+    // Get local date / time
+    LocalDate date = LocalDate.now();
+    LocalTime time = LocalTime.now();
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        System.out.println(line);
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println(e.getMessage());
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
+    // Get date time in UTC
+    Instant instant = Instant.now();
+
+    System.out.println(date);
+    System.out.println(time);
+    System.out.println(instant);
+
+    // Format date time
+    LocalDateTime datetime = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    String formattedDateTime = datetime.format(formatter);
+
+    System.out.println(formattedDateTime);
+
+    customDateTime();
+  }
+
+  static void customDateTime() {
+    // Custom date
+    LocalDate date = LocalDate.of(2026, 02, 12);
+
+    System.out.println(date);
+
+    // Custom date time
+    LocalDateTime date1 = LocalDateTime.of(2026, 02, 12, 12, 0, 0);
+    LocalDateTime date2 = LocalDateTime.of(2026, 02, 12, 13, 0, 0);
+
+    System.out.println(date1);
+    System.out.println(date2);
+
+    if (date1.isBefore(date2)) {
+      System.out.println(date1 + " is earlier than " + date2);
     }
 
   }
