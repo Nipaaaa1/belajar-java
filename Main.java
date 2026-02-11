0d1bfb70373708d6b76611d015f0d7ed2eb714a5
Nipaaaa1 <frostone224@gmail.com>
2026-02-11 20:34:27 +0000

    38: timer and timer task

--- Main.java
+++ Main.java
@@ -1,17 +1,40 @@
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
   public static void main(String[] args) {
-    // 37 - anonymous class
+    // 38 - timer and timertask
 
-    Person person1 = new Person("Udin");
-    Person person2 = new Person("Jamal") {
-      @Override
-      void greetings() {
-        System.out.println("Hi, I'm " + this.name);
-      }
-    };
+    Timer timer = new Timer();
 
-    person1.greetings();
-    person2.greetings();
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      System.out.print("Enter the # to countdown: ");
+      int countdown = scanner.nextInt();
+
+      TimerTask task = new TimerTask() {
+        int count = countdown;
+
+        @Override
+        public void run() {
+
+          if (count <= 0) {
+            System.out.println("Happy new year!");
+            timer.cancel();
+          } else {
+            System.out.println(count);
+            count--;
+          }
+        }
+      };
+
+      timer.schedule(task, 0, 1000);
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
   }
 }
